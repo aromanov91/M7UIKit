cf1a746838259d18658006966bf401bca9a2c4e6
Alexandr Romanov <aromanov07@gmail.com>
2019-10-19 23:18:21 +0300

    first commit

---
+++ M7UIKit/M7UIKit.h
@@ -0,0 +1,19 @@
+//
+//  M7UIKit.h
+//  M7UIKit
+//
+//  Created by Aleksandr Romanov on 19.10.2019.
+//  Copyright © 2019 Aleksandr Romanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for M7UIKit.
+FOUNDATION_EXPORT double M7UIKitVersionNumber;
+
+//! Project version string for M7UIKit.
+FOUNDATION_EXPORT const unsigned char M7UIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <M7UIKit/PublicHeader.h>
+
+
---
+++ M7UIKitExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  M7UIKitExamples
+//
+//  Created by Aleksandr Romanov on 19.10.2019.
+//  Copyright © 2019 Aleksandr Romanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
