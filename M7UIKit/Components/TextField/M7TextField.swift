24ddd82fbd10bdb34d59c16d6f86e4b480768dde
Alexandr Romanov <aromanov07@gmail.com>
2019-11-24 18:57:09 +0300

    Add none style

--- M7UIKit/Components/TextField/M7TextField.swift
+++ M7UIKit/Components/TextField/M7TextField.swift
@@ -12,11 +12,10 @@ public enum N7TextFieldType: String {
     
     case outlined = "outlined"
     
-    // Material
     case filled = "filled"
     
+    case none = "none"
 }
-
 
 @IBDesignable
 public class M7TextField: UITextField,UITextFieldDelegate {
@@ -37,8 +36,23 @@ public class M7TextField: UITextField,UITextFieldDelegate {
         }
     }
     
+    @IBInspectable public var errorText: String = "" {
+        didSet {
+            if errorText != oldValue {
+                normal()
+            }
+        }
+    }
+    
     public let title: M7Label = {
         var label = M7Label(font: M7LabelStyle.body2, color: M7LabelColor.primary)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    public let errorLabel: M7Label = {
+        var label = M7Label(font: M7LabelStyle.bodyBold2, color: M7LabelColor.error)
         label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -92,7 +106,7 @@ public class M7TextField: UITextField,UITextFieldDelegate {
                 // Radius
                 layer.cornerRadius = DesignSystem.radius.s
                 
-
+                
             } else {
                 font = M7Fonts.bodyBold1
                 
@@ -127,10 +141,15 @@ public class M7TextField: UITextField,UITextFieldDelegate {
             // Border
             layer.borderWidth = DesignSystem.border.Width.m
             layer.borderColor =  M7Colors.white.cgColor
-
             
+        case "none":
+            font = M7Fonts.bodyBold1
         default:
             break
+        }
+        
+        if errorText != "" {
+            self.addSubview(errorLabel)
         }
         
         layer.masksToBounds = true
@@ -167,12 +186,11 @@ public class M7TextField: UITextField,UITextFieldDelegate {
             layer.borderWidth = DesignSystem.border.Width.m
             layer.borderColor =  M7Colors.gray.cgColor
         case "filled":
-             base.backgroundColor = M7Colors.gray3
+            base.backgroundColor = M7Colors.gray3
             
         default:
             break
         }
-        
     }
     
     required init(coder aDecoder: NSCoder) {
@@ -213,20 +231,17 @@ public class M7TextField: UITextField,UITextFieldDelegate {
     }
     
     public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        print("textFieldShouldClear = хотите убрать клавиаутуру?")
-        
         resignFirstResponder() //Убрать клавиатуру
-        
         return true
     }
     
     override open func borderRect(forBounds bounds: CGRect) -> CGRect {
         let rect = CGRect(
-             x: 0,
-             y: 0,
-             width: bounds.size.width,
-             height: 200)
-         return rect
+            x: 0,
+            y: 0,
+            width: bounds.size.width,
+            height: 76)
+        return rect
     }
     
     override public func textRect(forBounds: CGRect) -> CGRect {
@@ -242,27 +257,26 @@ public class M7TextField: UITextField,UITextFieldDelegate {
             } else {
                 let rect = CGRect(
                     x: 16,
-                    y: 12,
+                    y: 18,
                     width: bounds.size.width - 20,
-                    height: 48)
+                    height: 64)
                 return rect
             }
             
-            
-            
-            
-//            let rect = CGRect(
-//                x: 16,
-//                y: 40,
-//                width: bounds.size.width - 20,
-//                height: 48)
-//            return rect
         case "filled":
             let rect = CGRect(
                 x: 2,
                 y: 30,
                 width: bounds.size.width,
                 height: 48)
+            return rect
+            
+        case "none":
+            let rect = CGRect(
+                x: 0,
+                y: 18,
+                width: bounds.size.width,
+                height: 64)
             return rect
             
         default:
@@ -278,16 +292,16 @@ public class M7TextField: UITextField,UITextFieldDelegate {
             if showLabel {
                 let rect = CGRect(
                     x: 16,
-                    y: 24,
+                    y: 40,
                     width: bounds.size.width - 20,
                     height: 48)
                 return rect
             } else {
                 let rect = CGRect(
                     x: 16,
-                    y: 12,
+                    y: 18,
                     width: bounds.size.width - 20,
-                    height: 48)
+                    height: 64)
                 return rect
             }
             
@@ -298,7 +312,13 @@ public class M7TextField: UITextField,UITextFieldDelegate {
                 width: bounds.size.width,
                 height: 48)
             return rect
-            
+        case "none":
+            let rect = CGRect(
+                x: 0,
+                y: 18,
+                width: bounds.size.width,
+                height: 64)
+            return rect
         default:
             break
         }
@@ -329,21 +349,24 @@ public class M7TextField: UITextField,UITextFieldDelegate {
                 y: 30,
                 width: bounds.size.width,
                 height: 24)
-             return rect
+            return rect
+        case "none":
+            let rect = CGRect(
+                x: 0,
+                y: 12,
+                width: bounds.size.width,
+                height: 24)
+            return rect
         default:
             break
         }
-         return CGRect(x: 20,y: 20, width: 20, height: 20)
+        return CGRect(x: 20,y: 20, width: 20, height: 20)
     }
-    
-    
     
     override open func clearButtonRect(forBounds bounds: CGRect) -> CGRect {
         let superRect = super.clearButtonRect(forBounds: bounds)
         return superRect.offsetBy(dx: -20, dy: 20)
     }
-    
-    
     
     override final public var borderStyle: UITextField.BorderStyle {
         get {
@@ -354,7 +377,6 @@ public class M7TextField: UITextField,UITextFieldDelegate {
         }
     }
     
-    
     override open var intrinsicContentSize: CGSize {
         if showLabel {
             return CGSize(width: bounds.size.width, height: 76)
@@ -362,5 +384,4 @@ public class M7TextField: UITextField,UITextFieldDelegate {
             return CGSize(width: bounds.size.width, height: 48)
         }
     }
-    
 }
